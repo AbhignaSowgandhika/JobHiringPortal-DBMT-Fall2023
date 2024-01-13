320b74a3698abaeae8826bae234988cfe0f297b1
AbhignaSowgandhika <69190803+AbhignaSowgandhika@users.noreply.github.com>
2024-01-12 18:41:00 -0600

    Add files via upload

---
+++ Code/constants/settings.php
@@ -0,0 +1,14 @@
+<?php
+$actual_link = $_SERVER['HTTP_HOST'];
+
+$default_timezone = 'America/Chicago';
+$smtp_host = 'smtp.gmail.com';
+$smtp_user = 'CareerConnect.group5@gmail.com';
+$smtp_pass = 'qtdx xwci tjuc nspo';
+
+$contact_mail = 'CareerConnect.group5@gmail.com';
+
+$fb  = '#';
+$tw = '#';
+$ig = '#';
+?>
